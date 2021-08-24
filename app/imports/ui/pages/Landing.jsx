16b4f85421872ee57e3eeff1dbb1cd58a68e9fcf
Jonathan-Ma <jma66@hawaii.edu>
2021-08-24 13:35:36 -1000

    Update Landing.jsx
    space blah

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 
+
+
+
+
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
