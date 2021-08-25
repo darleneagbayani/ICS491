e579bfd03d8e4435367f23f5f332b5e4451882b8
Jonathan-Ma <jma66@hawaii.edu>
2021-08-24 20:29:30 -1000

    Finished landing page
    Landing page done

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
-
+import { Button, Grid } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
@@ -8,13 +8,13 @@ class Landing extends React.Component {
       <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
         <img className="ui centered image" src={'/images/FULL LOGO.png'} alt={'Image not found'}/>
         <div className="ui buttons">
-          <button className="ui button" id="greenButton">
-                  Login
-          </button>
+          <Button as={NavLink} exact to='/signin' className="ui button" id="greenButton">
+                            Login
+          </Button>
           <div className="or"/>
-          <button className="ui button" id="blueButton">
-                  Signup
-          </button>
+          <Button as={NavLink} exact to={'/signup'} className="ui button" id="blueButton">
+                            Signup
+          </Button>
         </div>
       </Grid>
     );
