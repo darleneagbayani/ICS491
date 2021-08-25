43d6f560ff16894b9c731051f4d77ff30506e9c0
Jonathan-Ma <jma66@hawaii.edu>
2021-08-24 14:57:17 -1000

    added logo
    Implemented logo and buttons

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
       <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-          <img className="ui centered image" src={'/images/FULL LOGO.png'} alt={''}/>
-          <div className="ui buttons">
-              <button className="ui green button">
+        <img className="ui centered image" src={'/images/FULL LOGO.png'} alt={'Image not found'}/>
+        <div className="ui buttons">
+          <button className="ui button" id="greenButton">
                   Login
-              </button>
-              <div className="or"/>
-              <button className="ui blue button">
+          </button>
+          <div className="or"/>
+          <button className="ui button" id="blueButton">
                   Signup
-              </button>
-          </div>
-
+          </button>
+        </div>
       </Grid>
     );
   }
