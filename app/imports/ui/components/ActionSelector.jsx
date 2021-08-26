41c5fc032818a64b257b61c805c5a108255c574f
Rexter Delos Santos <rexterds@hawaii.edu>
2021-08-25 22:32:04 -1000

    updated action selector check-in to point to check-in page

--- app/imports/ui/components/ActionSelector.jsx
+++ app/imports/ui/components/ActionSelector.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Container, Grid, Image, Menu } from 'semantic-ui-react';
 import { withRouter, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class ActionSelector extends React.Component {
@@ -11,7 +14,7 @@ class ActionSelector extends React.Component {
     return (
       <div className="ui stackable three item menu" style={menuStyle}>
         <a className="active item" style={colStyle}>
-          <Container as={NavLink} activeClassName="active" exact to="/add" key='add'>
+          <Container as={NavLink} activeClassName="active" exact to={`/checkin/${this.props.currentUser}`} key='add'>
             <div className="row">
               <Image className="ui fluid middle aligned tiny image" src="/images/calendar.png"></Image>
             </div>
@@ -36,42 +39,19 @@ class ActionSelector extends React.Component {
           </Container>
         </a>
       </div>
-
-
-
-      // <div className="ui stackable equal width grid" textalign='center'>
-      //   <div className=" ui center aligned  column" style={colStyle}>
-      //     <Container as={NavLink} activeClassName="active" exact to="/add" key='add'>
-      //       <div className="row">
-      //         <Image className="ui fluid middle aligned tiny image" src="/images/calendar.png" ></Image>
-      //       </div>
-      //       <div className="row" style={textStyle}>DAILY CHECK IN</div>
-      //     </Container>
-      //   </div>
-      //   <div className="ui center aligned column" style={colStyle}>
-      //     <Container as={NavLink} activeClassName="active" exact to="/add" key='add'>
-      //       <div className="row">
-      //         <Image className="ui fluid middle aligned tiny image" src="/images/upload.png"></Image>
-      //
-      //       </div>
-      //       <div className="row" style={textStyle}>UPLOAD VACCINATION CARD</div>
-      //     </Container>
-      //   </div>
-      //
-      //
-      //   <div className=" ui center aligned column" style={colStyle}>
-      //     <Container>
-      //       <div className="center aligned row">
-      //         <Image className="ui fluid middle aligned tiny image" src="/images/history.png"></Image>
-      //         <div className="row" style={textStyle}>VIEW HISTORY</div>
-      //       </div>
-      //     </Container>
-      //   </div>
-      // </div>
-
-    )
-      ;
+    );
   }
 }
 
-export default ActionSelector;
+// Declare the types of all properties.
+ActionSelector.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+const ActionSelectorContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(ActionSelector);
+
+// Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter
+export default withRouter(ActionSelectorContainer);
