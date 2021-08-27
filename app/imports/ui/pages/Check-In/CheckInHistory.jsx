ee1b2bae8d20556efcd3c7560d255c64b0b29903
Rexter Delos Santos <rexterds@hawaii.edu>
2021-08-26 18:00:47 -1000

    Added basic functionality to the history page

--- app/imports/ui/pages/Check-In/CheckInHistory.jsx
+++ app/imports/ui/pages/Check-In/CheckInHistory.jsx
@@ -3,14 +3,34 @@ import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import { useParams } from 'react-router';
 import PropTypes from 'prop-types';
-import { Header } from 'semantic-ui-react';
+import { Container, Header, Loader, Table } from 'semantic-ui-react';
 import { CheckIn as CheckInCollection } from '../../../api/check-in/CheckIn';
+import HistoryItem from '../../components/Check-In/HistoryItem';
 
 class CheckInHistory extends React.Component {
 
   render() {
+    return (this.props.checkInReady) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
+    const { allCheckIns } = this.props;
     return (
-      <Header>Testing</Header>
+      <Container>
+        <Header>Daily Check-In History</Header>
+        <Table stackable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+              <Table.HeaderCell>Vaccination</Table.HeaderCell>
+              <Table.HeaderCell>Health Symptom</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            { allCheckIns.map((checkIn) => <HistoryItem key={checkIn._id} checkIn={checkIn}/>) }
+          </Table.Body>
+        </Table>
+      </Container>
     );
   }
 }
@@ -24,6 +44,7 @@ export default withTracker(() => {
   const checkInSubscribe = Meteor.subscribe(CheckInCollection.userPublicationName);
   const { username } = useParams();
   const allCheckIns = CheckInCollection.getAllCheckIns(username);
+  console.log(allCheckIns);
   return {
     checkInReady: checkInSubscribe.ready(),
     allCheckIns,
