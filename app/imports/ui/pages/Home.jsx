cd1de1e48673e19868af98e6cd2a9f80cc8d74c7
Rexter Delos Santos <rexterds@hawaii.edu>
2021-09-09 16:47:02 -1000

    cleaned up home.jsx

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -20,85 +20,70 @@ class Landing extends React.Component {
   }
 
   renderPage() {
-    const { recentCheckIn, recentCheckIn2, vaccineExists, username } = this.props;
-    console.log(recentCheckIn2);
-    // console.log(recentCheckIn2.firstDoseDate)
-    // console.log(typeof recentCheckIn2.firstDoseDate)
+    const { recentCheckIn, vaccineCheckIn, vaccineExists, username } = this.props;
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
     // if user is logged in return home pages
-    if (Meteor.userId()) {
-      return (
-        <Container id="landing-page" style={{ padding: '50px' }}>
-          <Grid textAlign="center" verticalAlign="middle" centered>
-            <Grid.Column mobile={16} tablet={8} computer={10}>
-              <Segment className="raised" >
-                <Header as="h2" textAlign="center">
+    return (
+      <Container id="landing-page" style={{ padding: '50px' }}>
+        <Grid textAlign="center" verticalAlign="middle" centered>
+          <Grid.Column mobile={16} tablet={8} computer={10}>
+            <Segment className="raised" >
+              <Header as="h2" textAlign="center">
                 Status
-                </Header>
-                <Grid textAlign="center" verticalAlign="middle" centered>
-                  <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
-                    <CheckInStatus
-                      status={'status' in recentCheckIn ? recentCheckIn.status : 'Not Clear'}
-                      vaccination={vaccineExists ? 'Approved' : 'Not Approved'}
-                      health={'health' in recentCheckIn ? recentCheckIn.health : 'Not Clear'}
-                    />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment className="raised" >
-                <Header as="h2" textAlign="center">
+              </Header>
+              <Grid textAlign="center" verticalAlign="middle" centered>
+                <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
+                  <CheckInStatus
+                    status={'status' in recentCheckIn ? recentCheckIn.status : 'Not Clear'}
+                    vaccination={vaccineExists ? 'Approved' : 'Not Approved'}
+                    health={'health' in recentCheckIn ? recentCheckIn.health : 'Not Clear'}
+                  />
+                </Grid.Column>
+              </Grid>
+            </Segment>
+            <Segment className="raised" >
+              <Header as="h2" textAlign="center">
                 Daily Check In
-                </Header>
-                <Grid textAlign="center" verticalAlign="middle" centered>
-                  <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
+              </Header>
+              <Grid textAlign="center" verticalAlign="middle" centered>
+                <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
                   Don't forget to do your daily check-in.
                   Help keep our community safe by completing your daily health check-in:
-                    <ol>
-                      <li>Check your symptoms.</li>
-                      <li>Keep track of your symptoms everyday.</li>
-                    </ol>
-                  </Grid.Column>
-                </Grid>
-                <Grid textAlign="center" verticalAlign="middle" centered>
-                  <Grid.Column textAlign="center">
-                    <Button id="btn-custom" content="Check Your Symptoms" as={NavLink} exact to={`/checkin/${username}`} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment className="raised" >
-                <Header as="h2" textAlign="center">
+                  <ol>
+                    <li>Check your symptoms.</li>
+                    <li>Keep track of your symptoms everyday.</li>
+                  </ol>
+                </Grid.Column>
+              </Grid>
+              <Grid textAlign="center" verticalAlign="middle" centered>
+                <Grid.Column textAlign="center">
+                  <Button id="btn-custom" content="Check Your Symptoms" as={NavLink} exact to={`/checkin/${username}`} />
+                </Grid.Column>
+              </Grid>
+            </Segment>
+            <Segment className="raised" >
+              <Header as="h2" textAlign="center">
                 Vaccination Card Submission
-                </Header>
-                <Grid textAlign="center" verticalAlign="middle" centered>
-                  <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
-                    <VaccineStatus
-                      vaccineName={recentCheckIn2 ? recentCheckIn2.vaccineName : 'No Submission'}
-                      firstDoseManufacturer={recentCheckIn2 ? recentCheckIn2.firstDoseManufacturer : 'No Submission'}
-                      dateString1={recentCheckIn2 ? recentCheckIn2.firstDoseDate.toLocaleDateString('en-US', options) : 'No Submission'}
-                      // dateString1={recentCheckIn2 ? recentCheckIn2.firstDoseDate :'No Submission'}
-                      firstDoseHealthcare={recentCheckIn2 ? recentCheckIn2.firstDoseHealthcare : 'No Submission'}
-                      secondDoseManufacturer={recentCheckIn2 ? recentCheckIn2.secondDoseManufacturer : 'No Submission'}
-                      dateString2={recentCheckIn2 ? recentCheckIn2.secondDoseDate.toLocaleDateString('en-US', options) : 'No Submission'}
-                      // dateString2={recentCheckIn2 ? recentCheckIn2.secondDoseDate :'No Submission'}
-                      secondDoseHealthcare={recentCheckIn2 ? recentCheckIn2.secondDoseHealthcare : 'No Submission'}
-
-                      // vaccineName={recentCheckIn2 ? recentCheckIn2.vaccineName :'No Submission'}
-                      // firstDoseManufacturer={recentCheckIn2 ? recentCheckIn2.firstDoseManufacturer :'No Submission'}
-                      // dateString1={recentCheckIn2 ? recentCheckIn2.firstDoseDate :'No Submission'}
-                      // firstDoseHealthcare={recentCheckIn2 ? recentCheckIn2.firstDoseHealthcare :'No Submission'}
-                      // secondDoseManufacturer={recentCheckIn2 ? recentCheckIn2.secondDoseManufacturer :'No Submission'}
-                      // dateString2={recentCheckIn2 ? recentCheckIn2.secondDoseDate :'No Submission'}
-                      // secondDoseHealthcare={recentCheckIn2 ? recentCheckIn2.secondDoseHealthcare :'No Submission'}
-                    />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-            </Grid.Column>
-          </Grid>
-        </Container>
-      );
-    }
+              </Header>
+              <Grid textAlign="center" verticalAlign="middle" centered>
+                <Grid.Column textAlign="left" mobile={15} tablet={15} computer={13}>
+                  <VaccineStatus
+                    vaccineName={vaccineCheckIn ? vaccineCheckIn.vaccineName : 'No Submission'}
+                    firstDoseManufacturer={vaccineCheckIn ? vaccineCheckIn.firstDoseManufacturer : 'No Submission'}
+                    dateString1={vaccineCheckIn ? vaccineCheckIn.firstDoseDate.toLocaleDateString('en-US', options) : 'No Submission'}
+                    firstDoseHealthcare={vaccineCheckIn ? vaccineCheckIn.firstDoseHealthcare : 'No Submission'}
+                    secondDoseManufacturer={vaccineCheckIn ? vaccineCheckIn.secondDoseManufacturer : 'No Submission'}
+                    dateString2={vaccineCheckIn ? vaccineCheckIn.secondDoseDate.toLocaleDateString('en-US', options) : 'No Submission'}
+                    secondDoseHealthcare={vaccineCheckIn ? vaccineCheckIn.secondDoseHealthcare : 'No Submission'}
+                  />
+                </Grid.Column>
+              </Grid>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
   }
 }
 
